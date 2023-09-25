081c7b3884db431564c2cd32be60f7d3bc5dd78e
Olivia Schif <131719296+livschif@users.noreply.github.com>
2023-09-25 15:37:33 -0500

    trying to fix deployment bug

--- server.js
+++ server.js
@@ -37,5 +37,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('http://localhost:' + PORT));
+    app.listen(PORT, () => console.log('Now listening'));
   });
+
+// sequelize.sync({ force: false }).then(() => {
+//     app.listen(PORT, () => console.log('http://localhost:' + PORT));
+//   });
